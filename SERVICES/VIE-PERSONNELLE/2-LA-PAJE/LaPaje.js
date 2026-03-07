f2dd9dae70c6368308be2bd8c1939bfbff107860
Elise Degardin <elise.degardin.mtd@proton.me>
2026-03-07 12:30:47 +0100

    Forget connection pop up js

--- SERVICES/VIE-PERSONNELLE/2-LA-PAJE/LaPaje.js
+++ SERVICES/VIE-PERSONNELLE/2-LA-PAJE/LaPaje.js
@@ -1,3 +1,26 @@
+//VARIABLES DES BOUTONS DU FORMULAIRE DE CONNEXION
+const Connexion = document.querySelector('.BoutonConnexion');
+const PopUp = document.querySelector('.PopUpConnexion');
+const Fermer = document.querySelector('.FermerConnexion');
+const FormulaireConnexion = document.querySelector('.FormulaireConnexion');
+
+//OUVRIR POP UP DE CONNEXION
+Connexion.addEventListener('click', () => {
+    PopUp.style.display = 'block';   
+});
+
+//FERMER POP UP DE CONNEXION
+Fermer.addEventListener('click', () => {
+   PopUp.style.display = 'none'; 
+});
+
+//FERMER POP UP EN CLIQUANT EN DEHORS DE CELLE-CI
+window.addEventListener('click', (e) => {
+    if (e.target === PopUp) {
+        PopUp.style.display = 'none';
+    }
+});
+
 /**************************FAQ DETTE****************************/
 
 const FaqQuestionAdoption = document.querySelector('.FaqQuestionAdoption');
