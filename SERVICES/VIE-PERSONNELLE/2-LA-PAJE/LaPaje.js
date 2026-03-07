3421297f33fed447e823d63e679e1d58c18f98d0
Elise Degardin <elise.degardin.mtd@proton.me>
2026-03-07 12:48:42 +0100

    Modified Paje html css and js files

--- SERVICES/VIE-PERSONNELLE/2-LA-PAJE/LaPaje.js
+++ SERVICES/VIE-PERSONNELLE/2-LA-PAJE/LaPaje.js
@@ -23,56 +23,56 @@ window.addEventListener('click', (e) => {
 
 /**************************FAQ DETTE****************************/
 
-const FaqQuestionAdoption = document.querySelector('.FaqQuestionAdoption');
-const FaqReponseAdoption = document.querySelector('.FaqReponseAdoption');
-const FaqQuestionNaissance = document.querySelector('.FaqQuestionNaissance');
-const FaqReponseNaissance = document.querySelector('.FaqReponseNaissance');
-const FaqQuestionGrossesse = document.querySelector('.FaqQuestionGrossesse');
-const FaqReponseGrossesse = document.querySelector('.FaqReponseGrossesse');
-const FaqQuestionPrepare = document.querySelector('.FaqQuestionPrepare');
-const FaqReponsePrepare = document.querySelector('.FaqReponsePrepare');
-const FaqQuestionActivite = document.querySelector('.FaqQuestionActivite');
-const FaqReponseActivite = document.querySelector('.FaqReponseActivite');
+const FaqQuestion1 = document.querySelector('.FaqQuestion1');
+const FaqReponse1 = document.querySelector('.FaqReponse1');
+const FaqQuestion2 = document.querySelector('.FaqQuestion2');
+const FaqReponse2 = document.querySelector('.FaqReponse2');
+const FaqQuestion3 = document.querySelector('.FaqQuestion3');
+const FaqReponse3 = document.querySelector('.FaqReponse3');
+const FaqQuestion4 = document.querySelector('.FaqQuestion4');
+const FaqReponse4 = document.querySelector('.FaqReponse4');
+const FaqQuestion5 = document.querySelector('.FaqQuestion5');
+const FaqReponse5 = document.querySelector('.FaqReponse5');
 
 //OUVRIR REPONSES
-FaqQuestionAdoption.addEventListener('click', () => {
-    FaqReponseAdoption.style.display = 'block';
-    FaqReponseNaissance.style.display = 'none';
-    FaqReponseGrossesse.style.display = 'none';
-    FaqReponsePrepare.style.display = 'none';
-    FaqReponseActivite.style.display = 'none';
+FaqQuestion1.addEventListener('click', () => {
+    FaqReponse1.style.display = 'block';
+    FaqReponse2.style.display = 'none';
+    FaqReponse3.style.display = 'none';
+    FaqReponse4.style.display = 'none';
+    FaqReponse5.style.display = 'none';
 });
 
-FaqQuestionNaissance.addEventListener('click', () => {
-    FaqReponseAdoption.style.display = 'none';
-    FaqReponseNaissance.style.display = 'block';
-    FaqReponseGrossesse.style.display = 'none';
-    FaqReponsePrepare.style.display = 'none';
-    FaqReponseActivite.style.display = 'none';
+FaqQuestion2.addEventListener('click', () => {
+    FaqReponse1.style.display = 'none';
+    FaqReponse2.style.display = 'block';
+    FaqReponse3.style.display = 'none';
+    FaqReponse4.style.display = 'none';
+    FaqReponse5.style.display = 'none';
 });
 
-FaqQuestionGrossesse.addEventListener('click', () => {
-    FaqReponseAdoption.style.display = 'none';
-    FaqReponseNaissance.style.display = 'none';
-    FaqReponseGrossesse.style.display = 'block';
-    FaqReponsePrepare.style.display = 'none';
-    FaqReponseActivite.style.display = 'none';
+FaqQuestion3.addEventListener('click', () => {
+    FaqReponse1.style.display = 'none';
+    FaqReponse2.style.display = 'none';
+    FaqReponse3.style.display = 'block';
+    FaqReponse4.style.display = 'none';
+    FaqReponse5.style.display = 'none';
 });
 
-FaqQuestionPrepare.addEventListener('click', () => {
-    FaqReponseAdoption.style.display = 'none';
-    FaqReponseNaissance.style.display = 'none';
-    FaqReponseGrossesse.style.display = 'none';
-    FaqReponsePrepare.style.display = 'block';
-    FaqReponseActivite.style.display = 'none';
+FaqQuestion4.addEventListener('click', () => {
+    FaqReponse1.style.display = 'none';
+    FaqReponse2.style.display = 'none';
+    FaqReponse3.style.display = 'none';
+    FaqReponse4.style.display = 'block';
+    FaqReponse5.style.display = 'none';
 });
 
-FaqQuestionActivite.addEventListener('click', () => {
-    FaqReponseAdoption.style.display = 'none';
-    FaqReponseNaissance.style.display = 'none';
-    FaqReponseGrossesse.style.display = 'none';
-    FaqReponsePrepare.style.display = 'none';
-    FaqReponseActivite.style.display = 'block';
+FaqQuestion5.addEventListener('click', () => {
+    FaqReponse1.style.display = 'none';
+    FaqReponse2.style.display = 'none';
+    FaqReponse3.style.display = 'none';
+    FaqReponse4.style.display = 'none';
+    FaqReponse5.style.display = 'block';
 });
 
 /*****************************************/
