e10b0301baf6ca52760adfdb94eecfdcd2c3b0d6
Elise <elise.degardin.mtd@proton.me>
2026-03-31 00:08:41 +0200

    Ajout et finition des pages html, css et JS de Handicap

---
+++ SERVICES/HANDICAP/Handicap.js
@@ -0,0 +1,24 @@
+/****************FORMULAIRE DE CONNEXION****************/
+
+//VARIABLES DES BOUTONS DU FORMULAIRE DE CONNEXION
+const Connexion = document.querySelector('.BoutonConnexion');
+const PopUp = document.querySelector('.PopUpConnexion');
+const Fermer = document.querySelector('.FermerConnexion');
+const FormulaireConnexion = document.querySelector('.FormulaireConnexion');
+
+//OUVRIR POP UP DE CONNEXION
+Connexion.addEventListener('click', () => {
+    PopUp.style.display = 'block';
+});
+
+//FERMER POP UP DE CONNEXION
+Fermer.addEventListener('click', () => {
+    PopUp.style.display = 'none';
+});
+
+//FERMER POP UP EN CLIQUANT EN DEHORS DE CELLE-CI
+window.addEventListener('click', (e) => {
+    if (e.target === PopUp) {
+        PopUp.style.display = 'none';
+    }
+});
